023b59dd340400cc026fbde2464315d89e9092f3
f20231229-ops <f20231229@goa.bits-pilani.ac.in>
2026-06-02 21:39:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,6 +135,11 @@ def not_found(_):
     return render_template("404.html"), 404
 
 
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
